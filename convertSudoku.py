c3a8c8974aeea8f4b4c0d998ad6d43cd22613dc1
Andrew Baluyut <andrew.baluyut@gmail.com>
2020-11-18 19:31:10 -0900

    working displayed result

--- convertSudoku.py
+++ convertSudoku.py
@@ -1,15 +1,5 @@
-## Solve Every Sudoku Puzzle
 
-## See http://norvig.com/sudoku.html
-
-## Throughout this program we have:
-##   r is a row,    e.g. 'A'
-##   c is a column, e.g. '3'
-##   s is a square, e.g. 'A3'
-##   d is a digit,  e.g. '9'
-##   u is a unit,   e.g. ['A1','B1','C1','D1','E1','F1','G1','H1','I1']
-##   grid is a grid,e.g. 81 non-blank chars, e.g. starting with '.18...7...
-##   values is a dict of possible values, e.g. {'A1':'12349', 'A2':'8', ...}
+import time, random
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
@@ -27,21 +17,6 @@ units = dict((s, [u for u in unitlist if s in u])
 peers = dict((s, set(sum(units[s],[]))-set([s]))
              for s in squares)
 
-################ Unit Tests ################
-
-def test():
-    "A set of tests that must pass."
-    assert len(squares) == 81
-    assert len(unitlist) == 27
-    assert all(len(units[s]) == 3 for s in squares)
-    assert all(len(peers[s]) == 20 for s in squares)
-    assert units['C2'] == [['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
-                           ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
-                           ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']]
-    assert peers['C2'] == set(['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2',
-                               'C1', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
-                               'A1', 'A3', 'B1', 'B3'])
-    print ('All tests pass.')
 
 ################ Parse a Grid ################
 
@@ -110,90 +85,43 @@ def display(values):
 
 ################ Search ################
 
-def solve(grid): return search(parse_grid(grid))
+def solve(grid): 
+    return search(parse_grid(grid))
 
 def search(values):
     "Using depth-first search and propagation, try all possible values."
     if values is False:
         return False ## Failed earlier
     if all(len(values[s]) == 1 for s in squares):
+        # display
         return values ## Solved!
     ## Chose the unfilled square s with the fewest possibilities
     n,s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
-    return some(search(assign(values.copy(), s, d))
-                for d in values[s])
 
-################ Utilities ################
-
-def some(seq):
     "Return some element of seq that is true."
+
+    seq = (search(assign(values.copy(), s, d)) for d in values[s])
+
     for e in seq:
         if e: return e
     return False
 
-def from_file(filename, sep='\n'):
-    "Parse a file into a list of strings, separated by sep."
-    f = open(filename, 'r')
-    return f.read().strip().split(sep)
-
-def shuffled(seq):
-    "Return a randomly shuffled copy of the input sequence."
-    seq = list(seq)
-    random.shuffle(seq)
-    return seq
-
-################ System test ################
-
-import time, random
-
-def solve_all(grids, name='', showif=0.0):
-    """Attempt to solve a sequence of grids. Report results.
-    When showif is a number of seconds, display puzzles that take longer.
-    When showif is None, don't display any puzzles."""
-    def time_solve(grid):
-        start = time.process_time()
-        values = solve(grid)        
-        t = time.process_time()-start
-        ## Display puzzles that take long enough
-        if showif is not None and t > showif:
-            display(grid_values(grid))
-            if values: display(values)
-            print ('(%.2f seconds)\n' % t)
-        return (t, solved(values))
-    times, results = zip(*[time_solve(grid) for grid in grids])
-    N = len(grids)
-    if N > 1:
-        print ("Solved %d of %d %s puzzles (avg %.2f secs (%d Hz), max %.2f secs)." % (
-            sum(results), N, name, sum(times)/N, N/sum(times), max(times)))
 
 def solved(values):
     "A puzzle is solved if each unit is a permutation of the digits 1 to 9."
-    def unitsolved(unit): return set(values[s] for s in unit) == set(digits)
+    def unitsolved(unit): 
+        return set(values[s] for s in unit) == set(digits)
     return values is not False and all(unitsolved(unit) for unit in unitlist)
-
-def random_puzzle(N=17):
-    """Make a random puzzle with N or more assignments. Restart on contradictions.
-    Note the resulting puzzle is not guaranteed to be solvable, but empirically
-    about 99.8% of them are solvable. Some have multiple solutions."""
-    values = dict((s, digits) for s in squares)
-    for s in shuffled(squares):
-        if not assign(values, s, random.choice(values[s])):
-            break
-        ds = [values[s] for s in squares if len(values[s]) == 1]
-        if len(ds) >= N and len(set(ds)) >= 8:
-            return ''.join(values[s] if len(values[s])==1 else '.' for s in squares)
-    return random_puzzle(N) ## Give up and make a new puzzle
 
 grid1  = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
 grid2  = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
 hard1  = '.....6....59.....82....8....45........3........6..3.54...325..6..................'
+
+def main():
+    display(solve(grid1))
     
 if __name__ == '__main__':
-    # test()
-    # solve_all(from_file("easy50.txt", '========'), "easy", None)
-    solve_all(from_file("top95.txt"), "hard", None)
-    solve_all(from_file("hardest.txt"), "hardest", None)
-    # solve_all([random_puzzle() for _ in range(99)], "random", 100.0)
+    main()
 
 ## References used:
 ## http://www.scanraid.com/BasicStrategies.htm
